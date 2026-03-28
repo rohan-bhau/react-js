68101a57b4e995fd44c3dbc488a45b0923c986ec
Rohan Mia <rohanmia.org@gmail.com>
2026-03-28 10:14:15 +0600

    nested routing

--- routing-advanced/src/App.jsx
+++ routing-advanced/src/App.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Product from "./pages/Product";
-import Footer from "./components/Footer";
-import { Route, Routes } from "react-router-dom";
-import PageNotFound from "./pages/PageNotFound";
-import Men from "./pages/Men";
-import Women from "./pages/Women";
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Footer from './components/Footer'
+import {Route, Routes  } from "react-router-dom";
+import PageNotFound from './pages/PageNotFound'
+import Men from './pages/Men'
+import Women from './pages/Women'
+
 
 const App = () => {
   return (
-    <div className="">
+    <div >
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -21,12 +22,11 @@ const App = () => {
           <Route path="men" element={<Men />} />
           <Route path="women" element={<Women />} />
         </Route>
-
         <Route path="*" element={<PageNotFound></PageNotFound>} />
       </Routes>
       <Footer></Footer>
     </div>
   );
-};
+}
 
-export default App;
+export default App
--- routing-advanced/src/pages/Product.jsx
+++ routing-advanced/src/pages/Product.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const Product = () => {
   return (
     <div>
           <div className='flex justify-center p-4 gap-10'>
-              <Link className='font-semibold text-xl' to='/product/men'>Men</Link>
-              <Link className='font-semibold text-xl' to='/product/women'>Women</Link>
-      </div>
+              
+              <Link className='text-xl font-semibold' to='/product/men'>Men</Link>
+              <Link className='text-xl font-semibold' to='/product/women'>Women</Link>
+          </div>
+          <Outlet></Outlet>
     </div>
   )
 }
